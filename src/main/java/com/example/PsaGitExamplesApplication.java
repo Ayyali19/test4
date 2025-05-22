27ad84c3c571ce9fc7a875ce47cc5255ee90ecdc
Ayyali19 <ayyalikalli@gmail.com>
2025-05-22 10:15:37 +0530

    Update PsaGitExamplesApplication.java
    something

--- src/main/java/com/example/PsaGitExamplesApplication.java
+++ src/main/java/com/example/PsaGitExamplesApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PsaGitExamplesApplication {
 
 	public static void main(String[] args) {
-		int x = 100;
+		int x = 10;
 		SpringApplication.run(PsaGitExamplesApplication.class, args);
 	}
 
